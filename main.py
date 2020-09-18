3100143a5c8e50bf7b979ef0dbccc5c915a30d16
originalmalek <56593369+originalmalek@users.noreply.github.com>
2020-09-18 15:18:48 +0300

    Moved variables, deleted repeated functions, new variable insta_account_name

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
 # -*- coding: utf8 -*-
 import argparse
-from instabot import Bot
 import os
-from dotenv import load_dotenv
 import re
+
+from dotenv import load_dotenv
+from instabot import Bot
 from random import choice
-
-load_dotenv()
-
-insta_login = os.getenv('INSTA_LOGIN')
-insta_pass = os.getenv('INSTA_PASS')
-bot = Bot()
-bot.login(username=insta_login, password=insta_pass)
 
 
 def is_user_exist(nicknames):
-    for nickname in nicknames:
-        if bot.get_user_id_from_username(nickname):
-            return True
-    return False
-
-
-def get_all_post_comments(post_link):
-    post_id = bot.get_media_id_from_link(post_link)
-    all_comments = bot.get_media_comments_all(post_id)
-
-    return all_comments
+    return any(bot.get_user_id_from_username(nickname) for nickname in nicknames)
 
 
 def get_commented_users(post_link):
     users = []
-    all_comments = get_all_post_comments(post_link)
+    all_comments = bot.get_media_comments_all(bot.get_media_id_from_link(post_link))
     for comment in all_comments:
         nicknames = re.findall('(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)',
                                comment['text'])
@@ -39,21 +23,11 @@ def get_commented_users(post_link):
     return set(users)
 
 
-def get_liked_users(post_link):
-    liked_users = bot.get_media_likers(bot.get_media_id_from_link(post_link))
-    return liked_users
-
-
-def get_following_users(insta_account_name):
-    following_users = bot.get_user_following(insta_account_name)
-    return following_users
-
-
 def get_performed_users(insta_account_name, post_link):
     performed_users = []
 
-    liked_users = get_liked_users(post_link)
-    following_users = get_following_users(insta_account_name)
+    liked_users = bot.get_media_likers(bot.get_media_id_from_link(post_link))
+    following_users = bot.get_user_following(insta_account_name)
     commented_users = get_commented_users(post_link)
     for id_user, nickname in commented_users:
         if id_user in liked_users and id_user in following_users:
@@ -68,7 +42,7 @@ def get_winner(insta_account_name, post_link):
 
 
 def main():
-    insta_account_name = 'wowbeautybar.ru'
+    insta_account_name = os.getenv('INSTA_GROUP_NAME')
 
     parser = argparse.ArgumentParser(description='Enter your post link')
     parser.add_argument('post_link', help='Enter post url')
@@ -79,4 +53,9 @@ def main():
 
 
 if __name__ == '__main__':
+    load_dotenv()
+    insta_login = os.getenv('INSTA_LOGIN')
+    insta_pass = os.getenv('INSTA_PASS')
+    bot = Bot()
+    bot.login(username=insta_login, password=insta_pass)
     main()
